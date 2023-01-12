e18a007fda88f6838e07bad7ede827345559e935
cagkanoztas <cagkan.oztas@jengal.com>
2023-01-12 13:08:14 +0300

    Updated types

--- projects/super-ui-lib/schematics/ng-add/index.ts
+++ projects/super-ui-lib/schematics/ng-add/index.ts
@@ -1,26 +1,28 @@
-import { Rule, Tree, SchematicContext, SchematicsException } from '@angular-devkit/schematics';
+import { Rule, Tree, SchematicContext, SchematicsException, UpdateRecorder } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { applyToUpdateRecorder } from '@schematics/angular/utility/change';
 import { addImportToModule } from '@schematics/angular/utility/ast-utils';
 import * as ts from 'typescript';
 
+type TreeBuffer = Buffer | null;
+
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
     context.logger.info('Adding library Module to the app...');
-    const modulePath = 'src/app/app.module.ts';
+    const modulePath: string = 'src/app/app.module.ts';
 
     if (!tree.exists(modulePath)) {
       throw new SchematicsException(`The file ${modulePath} doesn't exists!`);
     }
 
-    const recorder = tree.beginUpdate(modulePath);
+    const recorder: UpdateRecorder = tree.beginUpdate(modulePath);
+    const text: TreeBuffer = tree.read(modulePath);
 
-    const text = tree.read(modulePath);
     if(text === null) {
       throw new SchematicsException(`The file ${modulePath} doesn't exists!`)
     }
 
-    const source = ts.createSourceFile(
+    const source: ts.SourceFile = ts.createSourceFile(
       modulePath,
       text.toString(),
       ts.ScriptTarget.Latest,
@@ -30,6 +32,7 @@ export function ngAdd(): Rule {
     applyToUpdateRecorder(recorder,
         addImportToModule(source, modulePath, 'SuperUiLibModule', 'super-ui-lib')
     );
+
     tree.commitUpdate(recorder);
 
     context.logger.info('Installing dependencies...');
